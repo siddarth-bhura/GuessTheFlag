749ab96fe3221b94ddadb48fc5e1c6f0045902d2
Siddarth Bhura <sidjain.sb@gmail.com>
2025-09-03 12:06:21 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Siddarth Bhura on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
